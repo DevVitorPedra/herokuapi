62eda2c94be6bb80fc95fd9ece8537ac451bfbdd
DevVitorPedra <75690532+DevVitorPedra@users.noreply.github.com>
2021-12-12 14:29:52 -0300

    port

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const CONCURRENCY = process.env.WEB_CONCURRENCY || 1;
 const app = express()
+const port = process.env.PORT || 3333
 app.use(express.json())
-app.listen(5000,()=>console.log('rodando na 5000'))
+app.listen(port,()=>console.log('rodando na 5000'))
 app.get('/',(req,res)=>{
     res.json({message:"Rodando no heroku"})
 })
