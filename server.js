b1e72d856153f692a9c593ea759766c74f230652
DevVitorPedra <75690532+DevVitorPedra@users.noreply.github.com>
2021-12-12 14:15:58 -0300

    node start

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = express()
 app.use(express.json())
 app.listen(5000,()=>console.log('rodando na 5000'))
 app.get('/',(req,res)=>{
-    res.status(200).json({message:"Rodando no heroku"})
+    res.json({message:"Rodando no heroku"})
 })
